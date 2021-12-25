5019259668241baa9f8619407cd253adfca547b5
mason ko <gym1029@gmail.com>
2021-12-25 14:27:05 +0900

    add test log

--- app/main.go
+++ app/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+
 	"atos.com/shop/controller/http"
 	"atos.com/shop/repository"
 	"atos.com/shop/service"
@@ -8,6 +12,10 @@ import (
 )
 
 func main() {
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+	fmt.Println("load basepath " + basepath)
+
 	r := gin.Default()
 
 	shopRepo := repository.NewShopRepository()
